8aa4183d140de4a836c4ebc9930ef873023439a1
Shr3ne <shrinebloxianmail@gmail.com>
2026-06-15 17:07:15 +0530

    added parser navigation

--- src/grammar/parser.rs
+++ src/grammar/parser.rs
@@ -10,11 +10,27 @@ impl Parser {
     fn new(tokens: Vec<Token>) -> self {
         self {
             tokens,
-            0,
+            current: 0,
         }
     }
 
     fn get_current(&self) -> &Token {
-        &self.tokens[self.current]
+        &self.tokens[self.current];
     }
+
+    fn get_previous(&self) -> &Token {
+        &self.tokens[self.current - 1];
+    }
+
+    fn is_at_end(&self) -> bool {
+        self.get_current().token_type == TokenKind::Eof;
+    }
+
+    fn step(&mut self) -> &Token {
+        if !self.is_at_end() {
+            self.current += 1;
+        }
+        self.get_previous()
+    }
+
  }
